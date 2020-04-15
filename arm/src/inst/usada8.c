2313ca7eb63ce981a69c45ab1b7a5d1bad77a61c
yizhi <lujiankang@outlook.com>
2020-04-15 20:50:03 +0800

    完成绝对值加和无符号饱和相关指令。

--- arm/src/inst/usada8.c
+++ arm/src/inst/usada8.c
@@ -2,5 +2,31 @@
 
 int32 arm_inst_usada8(cpu_state_t *st, uint32 inst)
 {
-	return EXEC_NOT_IMPL;
+	loginst("usada8", inst);
+
+	if (!cond_ok())
+		return EXEC_SUCCESS;
+
+	uint32 rd = inst_b4(16);
+	uint32 rs = inst_b4(8);
+	uint32 rm = inst_b4(0);
+
+	uint8 rm0 = bitm(regv(rm), 0, 7);
+	uint8 rm8 = bitm(regv(rm), 8, 15);
+	uint8 rm16 = bitm(regv(rm), 16, 23);
+	uint8 rm24 = bitm(regv(rm), 24, 31);
+
+	uint8 rs0 = bitm(regv(rs), 0, 7);
+	uint8 rs8 = bitm(regv(rs), 8, 15);
+	uint8 rs16 = bitm(regv(rs), 16, 23);
+	uint8 rs24 = bitm(regv(rs), 24, 31);
+
+	uint32 diff1 = (rm0 < rs0) ? (rs0 - rm0) : (rm0 - rs0);
+	uint32 diff2 = (rm8 < rs8) ? (rs8 - rm8) : (rm8 - rs8);
+	uint32 diff3 = (rm16 < rs16) ? (rs16 - rm16) : (rm16 - rs16);
+	uint32 diff4 = (rm24 < rs24) ? (rs24 - rm24) : (rm24 - rs24);
+
+	regv(rd) = diff1 + diff2 + diff3 + diff4;
+
+	return EXEC_SUCCESS;
 }
--- arm/src/inst/usat16.c
+++ arm/src/inst/usat16.c
@@ -2,5 +2,27 @@
 
 int32 arm_inst_usat16(cpu_state_t *st, uint32 inst)
 {
-	return EXEC_NOT_IMPL;
+	loginst("usat16", inst);
+
+	if (!cond_ok())
+		return EXEC_SUCCESS;
+
+	//sbo
+	if (inst_b4(8) != 0xf)
+		return EXEC_UNPREDICTABLE;
+
+	uint32 sat_imm = inst_b4(16);
+	uint32 rd = inst_b4(12);
+	uint32 rm = inst_b4(0);
+
+	bool issat1, issat2;
+	uint16 rdl = unsigned_sat(bitm(regv(rm), 0, 15), sat_imm, &issat1);
+	uint16 rdh = unsigned_sat(bitm(regv(rm), 16, 31), sat_imm, &issat2);
+
+	regv(rd) = (rdh << 16) | rdl;
+
+	if (issat1 || issat2)
+		st->cpsr.q = 1;
+
+	return EXEC_SUCCESS;
 }
