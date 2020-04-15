524c433aa20523f233d8677ab0f7fb4eb3d4df53
yizhi <lujiankang@outlook.com>
2020-04-15 19:14:04 +0800

    完成测试指令

--- arm/src/inst/teq.c
+++ arm/src/inst/teq.c
@@ -2,5 +2,25 @@
 
 int32 arm_inst_teq(cpu_state_t *st, uint32 inst)
 {
-	return EXEC_NOT_IMPL;
+	loginst("teq", inst);
+
+	if (!cond_ok())
+		return EXEC_SUCCESS;
+
+	//sbz
+	if (inst_b4(12) != 0)
+		return EXEC_UNPREDICTABLE;
+
+	//必要值
+	uint32 rn = inst_b4(16);
+	uint32 operand;
+	bool carry = shifter_operand(st, inst, &operand);
+
+	uint32 alu_out = regv(rn) ^ operand;
+
+	st->cpsr.n = alu_out >> 31;
+	st->cpsr.z = alu_out == 0;
+	st->cpsr.c = carry;
+
+	return EXEC_SUCCESS;
 }
