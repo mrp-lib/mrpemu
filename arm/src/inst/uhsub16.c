a623ddaae95948a7220d2896219d70e0c4886e10
yizhi <lujiankang@outlook.com>
2020-04-15 19:57:51 +0800

    无符号减法指令完成

--- arm/src/inst/uhsub16.c
+++ arm/src/inst/uhsub16.c
@@ -2,5 +2,23 @@
 
 int32 arm_inst_uhsub16(cpu_state_t *st, uint32 inst)
 {
-	return EXEC_NOT_IMPL;
+	loginst("uhsub16", inst);
+
+	if (!cond_ok())
+		return EXEC_SUCCESS;
+
+	//sbo
+	if (inst_b4(8) != 0xf)
+		return EXEC_UNPREDICTABLE;
+
+	uint32 rn = inst_b4(16);
+	uint32 rd = inst_b4(12);
+	uint32 rm = inst_b4(0);
+
+	uint32 res0 = bitm(regv(rn), 0, 15) - bitm(regv(rm), 0, 15);
+	uint32 res16 = bitm(regv(rn), 16, 31) - bitm(regv(rm), 16, 31);
+
+	regv(rd) = (res0 >> 1) | ((res16 >> 1) << 16);
+
+	return EXEC_SUCCESS;
 }
--- arm/src/inst/usubaddx.c
+++ arm/src/inst/usubaddx.c
@@ -2,5 +2,29 @@
 
 int32 arm_inst_usubaddx(cpu_state_t *st, uint32 inst)
 {
-	return EXEC_NOT_IMPL;
+	loginst("usubaddx", inst);
+
+	if (!cond_ok())
+		return EXEC_SUCCESS;
+
+	//sbo
+	if (inst_b4(8) != 0xf)
+		return EXEC_UNPREDICTABLE;
+
+	uint32 rn = inst_b4(16);
+	uint32 rd = inst_b4(12);
+	uint32 rm = inst_b4(0);
+
+	uint32 ge;
+
+	uint32 res0 = bitm(regv(rn), 0, 15) + bitm(regv(rm), 16, 31);
+	if (carry16(res0))
+		ge |= 0b0011;
+	uint32 res16 = bitm(regv(rn), 16, 31) - bitm(regv(rm), 0, 15);
+	if (borrow16(res16))
+		ge |= 0b1100;
+
+	regv(rd) = res0 | (res16 << 16);
+
+	return EXEC_SUCCESS;
 }
