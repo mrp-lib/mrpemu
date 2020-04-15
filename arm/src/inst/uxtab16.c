cb50dd5991ea8dece176db97d50a3729e6e6b60e
yizhi <lujiankang@outlook.com>
2020-04-15 21:00:27 +0800

    扩展指定完成

--- arm/src/inst/uxtab16.c
+++ arm/src/inst/uxtab16.c
@@ -2,5 +2,25 @@
 
 int32 arm_inst_uxtab16(cpu_state_t *st, uint32 inst)
 {
-	return EXEC_NOT_IMPL;
+	loginst("uxtab16", inst);
+
+	if (!cond_ok())
+		return EXEC_SUCCESS;
+
+	//sbz
+	if (inst_bm(8, 9) != 0)
+		return EXEC_UNPREDICTABLE;
+
+	uint32 rn = inst_b4(16);
+	uint32 rd = inst_b4(12);
+	uint32 rm = inst_b4(0);
+	uint32 rotate = inst_bm(10, 11);
+
+	uint32 operand = ror(regv(rm), rotate << 3) & 0x000000ff;
+	uint16 resl = (uint16)bitm(regv(rn), 0, 15) + (uint16)bitm(operand, 0, 15);
+	uint16 resh = (uint16)bitm(regv(rn), 16, 31) + (uint16)bitm(operand, 16, 31);
+
+	regv(rd) = (resh << 16) | resl;
+
+	return EXEC_SUCCESS;
 }
--- arm/src/inst/uxth.c
+++ arm/src/inst/uxth.c
@@ -2,5 +2,20 @@
 
 int32 arm_inst_uxth(cpu_state_t *st, uint32 inst)
 {
-	return EXEC_NOT_IMPL;
+	loginst("uxth", inst);
+
+	if (!cond_ok())
+		return EXEC_SUCCESS;
+
+	//sbz
+	if (inst_bm(8, 9) != 0)
+		return EXEC_UNPREDICTABLE;
+
+	uint32 rd = inst_b4(12);
+	uint32 rm = inst_b4(0);
+	uint32 rotate = inst_bm(10, 11);
+
+	regv(rd) = ror(regv(rm), rotate << 3) & 0x0000ffff;
+
+	return EXEC_SUCCESS;
 }
